5a7588af65eb4232a274436fd66a06828c300d8b
uhammadziyo <DilafruzIminova71@gmail.com>
2024-09-14 09:04:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const name = prompt("ism kiriting:");
+const nameElement = document.createElement("div");
+nameElement.textContent = name;
+
+
+document.body.appendChild(nameElement);
---
+++ style.cs
@@ -0,0 +1,10 @@
+div {
+  font-size: 24px;
+  font-weight: bold;
+  color: blue;
+  margin-top: 20px;
+  align-items: center;
+}
+.you{
+  align-items: center;
+}
